d0048ad5064161e30ed07189720ac8f53f7f6176
kimiroo <43537287+kimiroo@users.noreply.github.com>
2026-04-03 23:06:29 +0900

    Purge email from credits dialog

---
+++ dialog/glossary_n_credits.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+@st.dialog('Glossary & Credits')
+def dialog_glossary_n_credits(glossary):
+
+    st.write('### Glossary')
+    st.write(glossary)
+
+    st.markdown('''
+        ### Credits
+        - Architecture: **Jee Lee**
+        - Code: **Yongjun Kim** [GitHub](https://github.com/kimiroo)
+    ''')
